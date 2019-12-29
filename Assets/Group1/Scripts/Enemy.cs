37e283e878e1649307e57f7e284b6ab262321357
Pekcapeh <58980761+Pekcapeh@users.noreply.github.com>
2019-12-29 16:09:15 +0700

    Start project and change enemy

--- Assets/Group1/Scripts/Enemy.cs
+++ Assets/Group1/Scripts/Enemy.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class e : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     private Vector3 Target;
-
-    // Start is called before the first frame update
+        
     void Start()
     {
         Target = Random.insideUnitCircle * 4;
     }
-
-    // Update is called once per frame
+        
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, Target, 2 * Time.deltaTime);
         if (transform.position == Target)
-            Target = Random.insideUnitCircle * 4    ;
+            Target = Random.insideUnitCircle * 4;
     }
 }
